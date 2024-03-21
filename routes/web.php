aa40a2b0bccd3eed99421215db45ec9ab8880ed8
wesleysoeliman <wesley.soeliman@gmail.com>
2024-03-21 14:21:54 +0100

    Recepten op homepagina + gebruiker van recept

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RecipesController;
 use Illuminate\Support\Facades\Route;
@@ -15,9 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('welcome');
 
 Route::get('/recipes', [RecipesController::class, 'index'])->name('recipes.index');
 Route::get('/recipes/create', [RecipesController::class, 'create'])->name('recipes.create');
